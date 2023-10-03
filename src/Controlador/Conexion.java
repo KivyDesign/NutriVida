4af5151ffcd52c19ec9abc97d34d9436c36fe795
germanesalvatierra <ge22679575@gmail.com>
2023-10-03 19:01:28 -0300

    actualizar

--- src/Controlador/Conexion.java
+++ src/Controlador/Conexion.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 public class Conexion {
 
     private static final String URL = "jdbc:mariadb://localhost/";
-    private static final String DB = "universidadulp";
+    private static final String DB = "nutrivida";
     private static final String USUARIO = "root";
     private static final String PASSWORD = "";
     private static Connection conexion = null;
--- src/Vista/NutriVida.java
+++ src/Vista/NutriVida.java
@@ -1,9 +1,29 @@
 package Vista;
 
+import Controlador.Conexion;
+import Controlador.PacienteData;
+import Modelo.Paciente;
+import java.sql.Connection;
+
 public class NutriVida {
 
     public static void main(String[] args) {
-        
+        // Realizo la coneccion a la DB
+        Connection con = Conexion.getConexion();
+
+        // Si la conexion fue exitosa lo informo como conectado con un (sout)
+        // para no tener ese molesto dialogo de conexion
+        if (con != null) {
+            System.out.println("Sistema de Gestión para la Universidad de La Punta - Estado: Conectado");
+        } else {
+            System.out.println("Sistema de Gestión para la Universidad de La Punta - Estado: Error");
+        }
+
+        //agrego un paciente nuevo
+        Paciente paciente = new Paciente(11112222,"Pablo Gomez","Colon 2332","123-456",true);
+        PacienteData pacData=new PacienteData();
+        pacData.guardarPaciente(paciente);
+
     }
-    
+
 }
