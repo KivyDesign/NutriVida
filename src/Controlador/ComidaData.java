00f855a6d9353fa2885f348018d8d043d282a459
arielMilton <arielmilton98@gmail.com>
2023-10-04 08:29:24 -0300

    Creo eliminarComida y modificarComida

--- src/Controlador/ComidaData.java
+++ src/Controlador/ComidaData.java
@@ -9,16 +9,17 @@ import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Statement;
 
 public class ComidaData {
+
     private Connection con;
     private ComidaData comidaData;
-    
-    public ComidaData(){
+
+    public ComidaData() {
         con = Conexion.getConexion();
     }
-    
-    public void guardarComida(Comida comida){
-        String sql = "INSERT INTO comida(nombre, detalle, cantCalorias, estado) VALUES(?, ?, ?, ?)";
+
+    public void guardarComida(Comida comida) {
         try {
+            String sql = "INSERT INTO comida(nombre, detalle, cantCalorias, estado) VALUES(?, ?, ?, ?)";
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, comida.getNombre());
             ps.setString(2, comida.getDetalle());
@@ -29,13 +30,39 @@ public class ComidaData {
             if (rs.next()) {
                 comida.setIdComida(rs.getInt(1));
             }
-            
+
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al insertar en la tabla Comida");
         }
-        
     }
-    
-    
+
+    public void eliminarComida(int idComida) {
+        try {
+            String sql = "UPDATE comida SET estado = 0 WHERE idComida = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idComida);
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Comida");
+        }
+    }
+
+    public void modificarComida(Comida comida) {
+        try {
+            String sql = "UPDATE comida SET nombre = ?, detalle = ?, cantCalorias = ? WHERE idComida = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, comida.getNombre());
+            ps.setString(2, comida.getDetalle());
+            ps.setInt(3, comida.getCalorias());
+            ps.setInt(4, comida.getIdComida());
+            ps.executeUpdate();
+            
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Comida");
+        }
+    }
 }
--- src/Vista/TestAriel.java
+++ src/Vista/TestAriel.java
@@ -21,10 +21,28 @@ public class TestAriel {
             System.out.println("Nutrivida - Estado: Error");
         }
         
-        // Inserto una comida
+        // Agrego una comida
         ComidaData comData = new ComidaData();
-        Comida com1 = new Comida("Manzana","Fruta",52,true);
-        comData.guardarComida(com1);
+//        Comida com1 = new Comida("Manzana","Fruta",52,true);
+//        Comida com2 = new Comida("Lenteja","legumbres",116,true);
+//        Comida com3 = new Comida("Banana","Fruta",89,true);
+//        Comida com4 = new Comida("Atún","Pescado",130,true);
+//        Comida com5 = new Comida("Zanahoria","Verdura",37,true);
+//        comData.guardarComida(com1);
+//        comData.guardarComida(com2);
+//        comData.guardarComida(com3);
+//        comData.guardarComida(com4);
+//        comData.guardarComida(com5);
+
+        // Elimino una comida
+//        comData.eliminarComida(4);
+
+        // Modifico una comida
+//        Comida comida = new Comida(1,"Frutilla","Es una fruta",33,false);
+//        comData.modificarComida(comida);
+
+        // Consulta de una comida
+        
     }
     
 }
