076c48b7e3b7509f7828bb350f8f06ceebec0e61
germanesalvatierra <ge22679575@gmail.com>
2023-10-06 18:50:34 -0300

    historialData

--- src/Modelo/Historial.java
+++ src/Modelo/Historial.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
+
+import java.time.LocalDate;
 
 /**
  *
  * @author german
  */
 public class Historial {
-    
+  private int idHistorial;
+  private Paciente paciente;
+  private double peso;
+  private LocalDate fechaConsulta;
+
+    public Historial() {
+    }
+
+    public Historial(Paciente paciente, double peso, LocalDate fechaConsulta) {
+        this.paciente = paciente;
+        this.peso = peso;
+        this.fechaConsulta = fechaConsulta;
+    }
+
+    public Historial(int idHistorial, Paciente paciente, double peso, LocalDate fechaConsulta) {
+        this.idHistorial = idHistorial;
+        this.paciente = paciente;
+        this.peso = peso;
+        this.fechaConsulta = fechaConsulta;
+    }
+
+    public int getIdHistorial() {
+        return idHistorial;
+    }
+
+    public void setIdHistorial(int idHistorial) {
+        this.idHistorial = idHistorial;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public LocalDate getFechaConsulta() {
+        return fechaConsulta;
+    }
+
+    public void setFechaConsulta(LocalDate fechaConsulta) {
+        this.fechaConsulta = fechaConsulta;
+    }
+
+    @Override
+    public String toString() {
+        return "Historial{" + "idHistorial=" + idHistorial + ", paciente=" + paciente + ", peso=" + peso + ", fechaConsulta=" + fechaConsulta + '}';
+    }
+  
 }
