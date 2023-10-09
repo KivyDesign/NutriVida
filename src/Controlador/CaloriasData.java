5dad4884813f159036bd904eb9e119ea0e4d88eb
Guido Sosa <todoelectrosolucion@gmail.com>
2023-10-09 00:24:25 -0300

    Actualizo

---
+++ src/Controlador/CaloriasData.java
@@ -0,0 +1,205 @@
+package Controlador;
+
+import Modelo.Calorias;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class CaloriasData {
+
+    private Connection con;
+
+    public CaloriasData() {
+        con = Conexion.getConexion();
+    }
+
+    /*
+     Estructura de la tabla calorias
+     int idCalorias
+     int idGrupoAlimenticio
+     String nombre
+     int calorias
+     int proteinas
+     int grasa
+     int carbohidratos
+     int fibra
+     int colesterol
+     Boolean estado
+     */
+    public void guardarCalorias(Calorias calorias) {
+        PreparedStatement ps = null;
+        try {
+            String sql = "INSERT INTO calorias(idGrupoAlimenticio, nombre, calorias, proteinas, grasa, carbohidratos, fibra, colesterol, estado) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1, calorias.getIdGrupoAlimenticio());
+            ps.setString(2, calorias.getNombre());
+            ps.setInt(3, calorias.getCalorias());
+            ps.setInt(4, calorias.getProteinas());
+            ps.setInt(5, calorias.getGrasa());
+            ps.setInt(6, calorias.getCarbohidratos());
+            ps.setInt(7, calorias.getFibra());
+            ps.setInt(8, calorias.getColesterol());
+            ps.setBoolean(9, true);
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                calorias.setIdCalorias(rs.getInt(1));
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al insertar en la tabla Calorias" + ex.getMessage());
+        }
+    }
+
+    public void eliminarCalorias(int idCalorias) {
+        PreparedStatement ps = null;
+        try {
+            String sql = "UPDATE calorias SET estado = 0 WHERE idCalorias = ?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, idCalorias);
+
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Calorias" + ex.getMessage());
+        }
+    }
+
+    public void modificarCalorias(Calorias calorias) {
+        PreparedStatement ps = null;
+        try {
+            String sql = "UPDATE calorias SET idGrupoAlimenticio = ?, nombre = ?, calorias = ?, proteinas = ?, grasa = ?, carbohidratos = ?, fibra = ?, colesterol = ?, estado = ? WHERE idCalorias = ?";
+
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1, calorias.getIdGrupoAlimenticio());
+            ps.setString(2, calorias.getNombre());
+            ps.setInt(3, calorias.getCalorias());
+            ps.setInt(4, calorias.getProteinas());
+            ps.setInt(5, calorias.getGrasa());
+            ps.setInt(6, calorias.getCarbohidratos());
+            ps.setInt(7, calorias.getFibra());
+            ps.setInt(8, calorias.getColesterol());
+            ps.setBoolean(9, calorias.getEstado());
+
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Calorias" + ex.getMessage());
+        }
+    }
+
+    public Calorias buscarCaloriasPorId(int idCalorias) {
+        Calorias calorias = null;
+        PreparedStatement ps = null;
+        try {
+            String sql = "SELECT idGrupoAlimenticio, nombre, calorias, proteinas, grasa, carbohidratos, fibra, colesterol, estado FROM calorias WHERE idCalorias = ?";
+
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1, idCalorias);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                calorias = new Calorias();
+                calorias.setIdCalorias(rs.getInt("idCalorias"));
+                calorias.setIdGrupoAlimenticio(rs.getInt("idGrupoAlimenticio"));
+                calorias.setNombre(rs.getString("nombre"));
+                calorias.setCalorias(rs.getInt("calorias"));
+                calorias.setProteinas(rs.getInt("proteinas"));
+                calorias.setGrasa(rs.getInt("grasa"));
+                calorias.setCarbohidratos(rs.getInt("carbohidratos"));
+                calorias.setFibra(rs.getInt("fibra"));
+                calorias.setFibra(rs.getInt("colesterol"));
+                calorias.setEstado(rs.getBoolean("estado"));
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Calorias" + ex.getMessage());
+        }
+        return calorias;
+    }
+
+    public ArrayList<Calorias> listarCaloriasPorLimites(int calMin, int calMax) {
+        ArrayList<Calorias> calorias = new ArrayList<>();
+        PreparedStatement ps = null;
+        try {
+            String sql = "SELECT idCalorias FROM calorias WHERE estado = 1 AND cantCalorias >= ? AND cantCalorias <= ?";
+
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1, calMin);
+            ps.setInt(2, calMax);
+
+            ResultSet rs = ps.executeQuery();
+
+            // Recorro el ResultSet y lo cargo en el Array calorias
+            while (rs.next()) {
+                calorias.add(buscarCaloriasPorId(rs.getInt("idCalorias")));
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "listarCalorias = Error al acceder a la tabla Calorias: " + ex.getMessage());
+        }
+        // Retorno el Array calorias con los valores de la consulta
+        return calorias;
+    }
+
+    public ArrayList<Calorias> listarCalorias() {
+        ArrayList<Calorias> calorias = new ArrayList<>();
+
+        try {
+            // La consulta funciona en MySQL
+            String sql = "SELECT * FROM calorias WHERE estado = 1 ORDER BY calorias";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            // Recorro el ResultSet y lo cargo en el Array calorias
+            while (rs.next()) {
+                Calorias caloria = new Calorias();
+
+                caloria.setIdCalorias(rs.getInt("idCalorias"));
+                caloria.setIdGrupoAlimenticio(rs.getInt("idGrupoAlimenticio"));
+                caloria.setNombre(rs.getString("nombre"));
+                
+                caloria.setIdCalorias(rs.getInt("idCalorias"));
+
+                calorias.add(caloria);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "listarCalorias = Error al acceder a la tabla Calorias: " + ex.getMessage());
+        }
+        // Retorno el Array calorias con los valores de la consulta
+        return calorias;
+    }
+
+    public void activarCalorias() {
+        PreparedStatement ps = null;
+        try {
+            String sql = "UPDATE calorias SET estado = 1 WHERE estado = 0";
+            ps = con.prepareStatement(sql);
+
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Calorias" + ex.getMessage());
+        }
+    }
+}
---
+++ src/Modelo/Calorias.java
@@ -0,0 +1,128 @@
+package Modelo;
+
+public class Calorias {
+    int idCalorias;
+    int idGrupoAlimenticio;
+    String nombre;
+    int calorias;
+    int proteinas;
+    int grasa;
+    int carbohidratos;
+    int fibra;
+    int colesterol;
+    Boolean estado;
+
+    public Calorias() {
+    }
+
+    public Calorias(int idGrupoAlimenticio, String nombre, int calorias, int proteinas, int grasa, int carbohidratos, int fibra, int colesterol, Boolean estado) {
+        this.idGrupoAlimenticio = idGrupoAlimenticio;
+        this.nombre = nombre;
+        this.calorias = calorias;
+        this.proteinas = proteinas;
+        this.grasa = grasa;
+        this.carbohidratos = carbohidratos;
+        this.fibra = fibra;
+        this.colesterol = colesterol;
+        this.estado = estado;
+    }
+
+    public Calorias(int idCalorias, int idGrupoAlimenticio, String nombre, int calorias, int proteinas, int grasa, int carbohidratos, int fibra, int colesterol, Boolean estado) {
+        this.idCalorias = idCalorias;
+        this.idGrupoAlimenticio = idGrupoAlimenticio;
+        this.nombre = nombre;
+        this.calorias = calorias;
+        this.proteinas = proteinas;
+        this.grasa = grasa;
+        this.carbohidratos = carbohidratos;
+        this.fibra = fibra;
+        this.colesterol = colesterol;
+        this.estado = estado;
+    }
+
+    public int getIdCalorias() {
+        return idCalorias;
+    }
+
+    public void setIdCalorias(int idCalorias) {
+        this.idCalorias = idCalorias;
+    }
+
+    public int getIdGrupoAlimenticio() {
+        return idGrupoAlimenticio;
+    }
+
+    public void setIdGrupoAlimenticio(int idGrupoAlimenticio) {
+        this.idGrupoAlimenticio = idGrupoAlimenticio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+
+    public int getProteinas() {
+        return proteinas;
+    }
+
+    public void setProteinas(int proteinas) {
+        this.proteinas = proteinas;
+    }
+
+    public int getGrasa() {
+        return grasa;
+    }
+
+    public void setGrasa(int grasa) {
+        this.grasa = grasa;
+    }
+
+    public int getCarbohidratos() {
+        return carbohidratos;
+    }
+
+    public void setCarbohidratos(int carbohidratos) {
+        this.carbohidratos = carbohidratos;
+    }
+
+    public int getFibra() {
+        return fibra;
+    }
+
+    public void setFibra(int fibra) {
+        this.fibra = fibra;
+    }
+
+    public int getColesterol() {
+        return colesterol;
+    }
+
+    public void setColesterol(int colesterol) {
+        this.colesterol = colesterol;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Calorias{" + "idCalorias=" + idCalorias + ", idGrupoAlimenticio=" + idGrupoAlimenticio + ", nombre=" + nombre + ", calorias=" + calorias + ", proteinas=" + proteinas + ", grasa=" + grasa + ", carbohidratos=" + carbohidratos + ", fibra=" + fibra + ", colesterol=" + colesterol + ", estado=" + estado + '}';
+    }
+
+}
