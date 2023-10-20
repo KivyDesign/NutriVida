494037b2519bbb2e13095158931db7135983733a
arielMilton <arielmilton98@gmail.com>
2023-10-20 19:56:05 -0300

    Actualizo AlimentosForm

--- src/Vista/AlimentosForm.java
+++ src/Vista/AlimentosForm.java
@@ -8,6 +8,7 @@ package Vista;
 import Controlador.ComidaData;
 import Controlador.Conexion;
 import Modelo.Comida;
+import java.awt.Color;
 import java.sql.Connection;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
@@ -20,6 +21,8 @@ public class AlimentosForm extends javax.swing.JFrame {
 
     private ComidaData comData;
     private DefaultTableModel modelo = new DefaultTableModel();
+    int l1 = 0;
+    int l2 = Integer.MAX_VALUE;
 
     /**
      * Creates new form AlimentosForm
@@ -28,6 +31,7 @@ public class AlimentosForm extends javax.swing.JFrame {
         initComponents();
         comData = new ComidaData();
         Connection con = Conexion.getConexion();
+        MensajeSB(1, "Ingrese un Limite Mínimo y Máximo de Calorías para el filtrado de Comidas");
         armarCabeceraDeLaTabla();
         cargarComidas();
     }
@@ -48,13 +52,13 @@ public class AlimentosForm extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
+        jlMensajeSB = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtComidas = new javax.swing.JTable();
         jLabel5 = new javax.swing.JLabel();
-        jtfLimiteInferior = new javax.swing.JTextField();
+        jtfLimiteMin = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        jtfLimiteSuperior = new javax.swing.JTextField();
+        jtfLimiteMax = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -122,9 +126,9 @@ public class AlimentosForm extends javax.swing.JFrame {
         jPanel4.setBackground(new java.awt.Color(31, 75, 128));
         jPanel4.setPreferredSize(new java.awt.Dimension(100, 48));
 
-        jLabel4.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("jLabel4");
+        jlMensajeSB.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        jlMensajeSB.setForeground(new java.awt.Color(255, 255, 255));
+        jlMensajeSB.setText("jLabel4");
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
@@ -132,14 +136,14 @@ public class AlimentosForm extends javax.swing.JFrame {
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel4)
+                .addComponent(jlMensajeSB)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel4)
+                .addComponent(jlMensajeSB)
                 .addContainerGap(15, Short.MAX_VALUE))
         );
 
@@ -158,7 +162,19 @@ public class AlimentosForm extends javax.swing.JFrame {
 
         jLabel5.setText("Calorias entre:");
 
+        jtfLimiteMin.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jtfLimiteMinKeyReleased(evt);
+            }
+        });
+
         jLabel6.setText("y:");
+
+        jtfLimiteMax.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jtfLimiteMaxKeyReleased(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -172,11 +188,11 @@ public class AlimentosForm extends javax.swing.JFrame {
                         .addGap(226, 226, 226)
                         .addComponent(jLabel5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jtfLimiteInferior, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jtfLimiteMin, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(jLabel6)
                         .addGap(18, 18, 18)
-                        .addComponent(jtfLimiteSuperior, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jtfLimiteMax, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(40, 40, 40)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 705, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -188,8 +204,8 @@ public class AlimentosForm extends javax.swing.JFrame {
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(55, 55, 55)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jtfLimiteInferior, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jtfLimiteSuperior, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtfLimiteMin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtfLimiteMax, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5)
                     .addComponent(jLabel6))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
@@ -211,6 +227,50 @@ public class AlimentosForm extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jtfLimiteMinKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtfLimiteMinKeyReleased
+
+        try {
+            if (!jtfLimiteMin.getText().isEmpty()) {
+                l1 = Integer.parseInt(jtfLimiteMin.getText());
+            }
+            if (!jtfLimiteMax.getText().isEmpty()) {
+                l2 = Integer.parseInt(jtfLimiteMax.getText());
+            }
+            MensajeSB(1, "");
+            borrarFilasTabla();
+            ArrayList<Comida> lista = comData.listarComidasCalorias(l1, l2);
+            for (Comida comida : lista) {
+                Object[] objComida = new Object[]{comida.getIdComida(), comida.getNombre(), comida.getDetalle(), comida.getCalorias()};
+                modelo.addRow(objComida);
+            }
+
+        } catch (NumberFormatException ex) {
+            MensajeSB(2, "El Limite Mínimo debe completarse solo con números");
+        }
+    }//GEN-LAST:event_jtfLimiteMinKeyReleased
+
+    private void jtfLimiteMaxKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtfLimiteMaxKeyReleased
+
+        try {
+            if (!jtfLimiteMin.getText().isEmpty()) {
+                l1 = Integer.parseInt(jtfLimiteMin.getText());
+            }
+            if (!jtfLimiteMax.getText().isEmpty()) {
+                l2 = Integer.parseInt(jtfLimiteMax.getText());
+            }
+            MensajeSB(1, "");
+            borrarFilasTabla();
+            ArrayList<Comida> lista = comData.listarComidasCalorias(l1, l2);
+            for (Comida comida : lista) {
+                Object[] objComida = new Object[]{comida.getIdComida(), comida.getNombre(), comida.getDetalle(), comida.getCalorias()};
+                modelo.addRow(objComida);
+            }
+
+        } catch (NumberFormatException ex) {
+            MensajeSB(2, "El Limite Maximo debe completarse solo con números");
+        }
+    }//GEN-LAST:event_jtfLimiteMaxKeyReleased
 
     /**
      * @param args the command line arguments
@@ -251,7 +311,6 @@ public class AlimentosForm extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
@@ -259,10 +318,32 @@ public class AlimentosForm extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel jlMensajeSB;
     private javax.swing.JTable jtComidas;
-    private javax.swing.JTextField jtfLimiteInferior;
-    private javax.swing.JTextField jtfLimiteSuperior;
+    private javax.swing.JTextField jtfLimiteMax;
+    private javax.swing.JTextField jtfLimiteMin;
     // End of variables declaration//GEN-END:variables
+
+    public void MensajeSB(int color, String mensaje) {
+
+        // Los valores pueden variar de 0 a 255
+        if (color == 1) {
+            // Si el color es igual a 1 entonces es = a verde
+            // En este caso Red = 0, Green = 153, Blue = 102.
+            jlMensajeSB.setForeground(new Color(255, 255, 255));
+        } else if (color == 2) {
+            // Si el color es igual a 2 entonces es = a rojo
+            // Los valores pueden variar de 0 a 255. En este caso Red = 153, Green = 51, Blue = 0.
+            jlMensajeSB.setForeground(new Color(255, 50, 0));
+        } else if (color == 3) {
+            // Usado al iniciar el Form para que no se vea el texto dummy 31, 75, 128
+            jlMensajeSB.setForeground(new Color(31, 75, 128));
+        }
+        // Aquí cargo el texto del mensaje en el Label
+        // Si el texto del mensaje esta vacio entonces no muestro texto en
+        // el Label pero limpio el texto anterior que pueda haber quedado
+        jlMensajeSB.setText(mensaje);
+    }
 
     public void borrarFilasTabla() {
         if (modelo != null) {
@@ -275,6 +356,7 @@ public class AlimentosForm extends javax.swing.JFrame {
             }
         }
     }
+
     public void armarCabeceraDeLaTabla() {
         modelo.addColumn("ID");
         modelo.addColumn("Nombre");
@@ -291,21 +373,11 @@ public class AlimentosForm extends javax.swing.JFrame {
 
     public void cargarComidas() {
         borrarFilasTabla();
-        
+
         ArrayList<Comida> lista = comData.listarComidas();
         for (Comida comida : lista) {
-            Object[] objComida = new Object[]{comida.getIdComida(),comida.getNombre(),comida.getDetalle(),comida.getCalorias()};
+            Object[] objComida = new Object[]{comida.getIdComida(), comida.getNombre(), comida.getDetalle(), comida.getCalorias()};
             modelo.addRow(objComida);
         }
-        System.out.println("listo");
-//            for (Comida comida : lista) {
-//                System.out.println("hola");
-//                System.out.println(comida.toString());
-//                modelo.addRow((Object[]) (Object) comida);
-//            }
-//        } catch (NullPointerException e) {
-//            System.out.println("no hay nada aca");
-//        }
-
     }
 }
